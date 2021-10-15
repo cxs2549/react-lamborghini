a6ea2c32062e006a97c66646e7e6988383848a70
Sanchez <sanchez.cliff.86@gmail.com>
2021-10-14 19:04:17 -0700

    devstart

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="px-5">
+      <h2>aventador lp 780-4 ultimate roadster</h2>
+      <h1>Based on a true story</h1>
+    </div>
+  )
+}
+
+export default App
